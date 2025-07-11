1a810b7ecd6b33bdb95ffcb86b2d9770b10d8d80
yaaabs <brianyabut02@gmail.com>
2025-07-12 00:42:20 +0800

    Disabled the timer sound and added BUG tag in Sounds

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = "customodoro-cache-v1";
-
+const CACHE_NAME = "focuskaya-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -7,30 +6,21 @@ const urlsToCache = [
   "/css/settings.css",
   "/js/script.js",
   "/js/settings.js",
+  "/images/Pomodoro.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
-      )
-    )
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
