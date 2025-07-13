e46e1e34564d9fc3299972db57520c8ce235b272
yaaabs <155513839+yaaabs@users.noreply.github.com>
2025-07-14 06:05:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "focuskaya-cache-v1";
+const CACHE_NAME = "customodoro-cache-v2.1.1";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -6,15 +6,28 @@ const urlsToCache = [
   "/css/settings.css",
   "/js/script.js",
   "/js/settings.js",
-  "/images/Pomodoro.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name)) 
+      );
+    })
+  );
+  self.clients.claim(); 
 });
 
 self.addEventListener("fetch", (event) => {
